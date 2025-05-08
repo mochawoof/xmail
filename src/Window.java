c4d705094e301acb8462bb1deb56dce2d20c486d
Julian <julianherbert.2007@gmail.com>
2025-05-08 10:36:44 -0400

    add libraries

---
+++ src/Window.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class Window extends JFrame {
+    public Window() {
+        setTitle("XMail");
+        setSize(800, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setVisible(true);
+    }
+}
