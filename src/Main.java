65e879f3dc96cec02030e8cd8dde5820e23bf3b7
Julian <julianherbert.2007@gmail.com>
2025-05-09 12:10:18 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import javax.swing.*;
 class Main {
     public static void main(String[] args) {
+        // Todo run on edt
         new Window();
     }
 }
--- src/Window.java
+++ src/Window.java
@@ -7,7 +7,26 @@ class Window extends JFrame {
         setTitle("XMail");
         setSize(800, 600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLaf("org.pushingpixels.substance.api.skin.SubstanceBusinessLookAndFeel");
 
         setVisible(true);
+    }
+
+    public void setLaf(String lafName) {
+        try {
+            for (UIManager.LookAndFeelInfo lafInfo : UIManager.getInstalledLookAndFeels()) {
+                if (lafInfo.getName().equals(lafName)) {
+                    UIManager.setLookAndFeel(lafInfo.getClassName());
+                    SwingUtilities.updateComponentTreeUI(this);
+                    return;
+                }
+            }
+
+            // Try to set laf directly
+            UIManager.setLookAndFeel(lafName);
+            SwingUtilities.updateComponentTreeUI(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
