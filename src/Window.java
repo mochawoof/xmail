5c2c13d68d828af36ab6451521275852165517fb
Julian <julianherbert.2007@gmail.com>
2025-05-12 12:33:33 -0400

    update window

--- src/Window.java
+++ src/Window.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+import org.pushingpixels.flamingo.api.ribbon.*;
+
 class Window extends JFrame {
     public Window() {
         setTitle("XMail");
